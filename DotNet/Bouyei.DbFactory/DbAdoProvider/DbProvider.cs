6fc21fdacb52473c32884269590e278d5a809527
bouyei <453840293@qq.com>
2019-01-08 10:01:44 +0800

    null auto filter mapping

--- DotNet/Bouyei.DbFactory/DbAdoProvider/DbProvider.cs
+++ DotNet/Bouyei.DbFactory/DbAdoProvider/DbProvider.cs
@@ -9,14 +9,11 @@
 using System;
 using System.Data;
 using System.Data.Common;
-using System.Linq;
 using System.Collections.Generic;
-using System.Threading;
 
 namespace Bouyei.DbFactory.DbAdoProvider
 {
     using DbUtils;
-    using System.Linq.Expressions;
 
     public class DbProvider : DbCommonBuilder, IDbProvider
     {
@@ -592,57 +589,5 @@ namespace Bouyei.DbFactory.DbAdoProvider
             return null;
         }
         #endregion
-    }
-
-    public static class DbProviderExtensions
-    {
-        public static DbResult<List<T>, string> Query<T>(this IDbProvider dbProvider,
-            Expression<Func<T, bool>> predicate) where T : class
-        {
-            ISqlProvider sql = SqlProvider.CreateProvider();
-            var commandText = sql.Select<T>().From<T>().Where(predicate).SqlString;
-            var rt = dbProvider.Query<T>(new Parameter(commandText));
-            if (rt.Info != string.Empty)
-                rt.Info = rt.Info + commandText;
-
-            return rt;
-        }
-
-        public static DbResult<int, string> Delete<T>(this IDbProvider dbProvider
-            , Expression<Func<T, bool>> predicate)
-        {
-            ISqlProvider sql = SqlProvider.CreateProvider();
-            var commandText = sql.Delete().From<T>().Where(predicate).SqlString;
-            var rt = dbProvider.ExecuteCmd(new Parameter(commandText));
-            if (rt.Info != string.Empty)
-                rt.Info = rt.Info + commandText;
-
-            return rt;
-        }
-
-        public static DbResult<int, string> Update<T>(this IDbProvider dbProvider,
-            T value, Expression<Func<T, bool>> predicate) where T : class
-        {
-            ISqlProvider sql = SqlProvider.CreateProvider();
-            var commandText = sql.Update<T>().Set<T>(value).Where(predicate).SqlString;
-            var rt = dbProvider.ExecuteCmd(new Parameter(commandText));
-
-            if (rt.Info != string.Empty)
-                rt.Info = rt.Info + commandText;
-
-            return rt;
-        }
-
-        public static DbResult<int, string> Insert<T>(this IDbProvider dbProvider, params T[] value) where T : class
-        {
-            ISqlProvider sql = SqlProvider.CreateProvider();
-            var commandText = sql.Insert<T>().Values<T>(value).SqlString;
-            var rt = dbProvider.ExecuteCmd(new Parameter(commandText));
-
-            if (rt.Info != string.Empty)
-                rt.Info = rt.Info + commandText;
-
-            return rt;
-        }
     }
 }
---
+++ DotNetCore/Bouyei.DbFactoryCore/DbAdoProvider/DbProviderExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Linq;
+
+namespace Bouyei.DbFactoryCore.DbAdoProvider
+{
+    public static class DbProviderExtensions
+    {
+        public static DbResult<List<T>, string> Query<T>(this IDbProvider dbProvider,
+            Expression<Func<T, bool>> predicate) where T : class
+        {
+            ISqlProvider sql = SqlProvider.CreateProvider();
+            var commandText = sql.Select<T>().From<T>().Where(predicate).SqlString;
+            var rt = dbProvider.Query<T>(new Parameter(commandText));
+            if (rt.Info != string.Empty)
+                rt.Info = rt.Info + commandText;
+
+            return rt;
+        }
+
+        public static DbResult<int, string> Delete<T>(this IDbProvider dbProvider
+            , Expression<Func<T, bool>> predicate)
+        {
+            ISqlProvider sql = SqlProvider.CreateProvider();
+            var commandText = sql.Delete().From<T>().Where(predicate).SqlString;
+            var rt = dbProvider.ExecuteCmd(new Parameter(commandText));
+            if (rt.Info != string.Empty)
+                rt.Info = rt.Info + commandText;
+
+            return rt;
+        }
+
+        public static DbResult<int, string> Update<T>(this IDbProvider dbProvider,
+            T value, Expression<Func<T, bool>> predicate) where T : class
+        {
+            ISqlProvider sql = SqlProvider.CreateProvider();
+            var commandText = sql.Update<T>().Set<T>(value).Where(predicate).SqlString;
+            var rt = dbProvider.ExecuteCmd(new Parameter(commandText));
+
+            if (rt.Info != string.Empty)
+                rt.Info = rt.Info + commandText;
+
+            return rt;
+        }
+
+        public static DbResult<int, string> Insert<T>(this IDbProvider dbProvider, params T[] value) where T : class
+        {
+            ISqlProvider sql = SqlProvider.CreateProvider();
+            var commandText = sql.Insert<T>().Values<T>(value).SqlString;
+            var rt = dbProvider.ExecuteCmd(new Parameter(commandText));
+
+            if (rt.Info != string.Empty)
+                rt.Info = rt.Info + commandText;
+
+            return rt;
+        }
+    }
+}
